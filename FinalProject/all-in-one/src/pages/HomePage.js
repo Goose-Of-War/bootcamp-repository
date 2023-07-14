efb1738506add9c1250b88ac3c728c113ed277ef
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-07-14 17:28:34 +0530

    Deets

--- FinalProject/all-in-one/src/pages/HomePage.js
+++ FinalProject/all-in-one/src/pages/HomePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const HomePage = () => {
-	return (<>
-		Hello
-	</>);
+	return (<div style={{ margin: '10px' }}>
+		<h1> Landing Page </h1>
+		<p> Click on the navbar links to go to different apps. </p>
+		<p> Submission by Vidunram A R </p>
+		<p><i> PS: All inputs work after you press 'Enter'. Clicking on 'ALL IN ONE' leads to this page. </i></p>
+	</div>);
 };
 
 export default HomePage;
